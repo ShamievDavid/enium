166f531ba2273b426852d1dc7460925c0172c6b3
david.shamiiev <david.shamiiev@rad4m.com>
2022-12-13 12:36:43 +0200

    add iphone

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import { ContactUs } from "./features/ContactUs/ContactUs";
 import { Main } from "./features/Main/Main";
@@ -12,7 +12,19 @@ function App() {
   const [lendingPartners, setLendingPartners] = useState(false);
   const [dealers, setDealers] = useState(false);
 
-  console.log("dealers", dealers);
+  useEffect(() => {
+    if (
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i)
+    ) {
+      /* iOS hides Safari address bar */
+      window.addEventListener("load", function () {
+        setTimeout(function () {
+          window.scrollTo(0, 1);
+        }, 1000);
+      });
+    }
+  }, []);
 
   const onContanctForm = () => {
     setShowForm(false);
