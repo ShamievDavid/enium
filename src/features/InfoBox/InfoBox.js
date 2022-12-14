0a985e4ccedb5854b81fa96871f491340f0cce70
david.shamiiev <david.shamiiev@rad4m.com>
2022-12-14 16:57:12 +0200

    mobile layout

--- src/features/InfoBox/InfoBox.js
+++ src/features/InfoBox/InfoBox.js
@@ -21,28 +21,29 @@ export const InfoBox = ({ showForm, setShowForm }) => {
         </div>
       </div>
       <div className="info-footer">
-        <div className="description">
-          <div className="description-title">ECG</div>
-          <div>
-            <div>
-              <br />
-              Enium is committed to presenting innovative ways for each customer
-              to be environmentally aware and fiscally responsible by providing
-              finance solutions with integrity to every customer one project at
-              a time. <br />
-              <br />
-              At Enium, we are passionate about connecting customers and
-              contractors with terrific loan programs to improve the quality of
-              life of all those within our reach.
-            </div>
+        <div className="info_footer_header">
+          <div className="infobox_img_container">
+            <img
+              className="logo"
+              src="./assets/images/enium_logo_black.png"
+              alt="logo"
+            />
           </div>
+          <div className="description-title">ECG</div>
         </div>
-        <div className="infobox_img_container">
-          <img
-            className="logo"
-            src="./assets/images/enium_logo.png"
-            alt="logo"
-          />
+        <div className="description">
+          <div className="empty_block"></div>
+          <div className="description_text">
+            <br />
+            Enium is committed to presenting innovative ways for each customer
+            to be environmentally aware and fiscally responsible by providing
+            finance solutions with integrity to every customer one project at a
+            time. <br />
+            <br />
+            At Enium, we are passionate about connecting customers and
+            contractors with terrific loan programs to improve the quality of
+            life of all those within our reach.
+          </div>
         </div>
       </div>
     </div>
