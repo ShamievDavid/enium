c424dc3a55d823625ccb104a601646e572b557a9
david.shamiiev <david.shamiiev@rad4m.com>
2022-12-20 14:31:31 +0200

    layout dealers

--- src/features/VideoBox/components/Dealers/Dealers.jsx
+++ src/features/VideoBox/components/Dealers/Dealers.jsx
@@ -4,13 +4,6 @@ import "./Dealers.scss";
 export const Dealers = ({ dealers, setDealers, showForm, setShowForm }) => {
   return (
     <div className={showForm ? "dealers_container_blur" : "dealers_container"}>
-      <div className="dealers_bg">
-        <img
-          className="dealers_bg_img"
-          alt="homeowners bg"
-          src="./assets/images/enium_dealers_bg.jpg"
-        />
-      </div>
       <div className="dealers_header">
         <div className="dealers_header_left">
           <div className="dealers_logo">
@@ -34,7 +27,6 @@ export const Dealers = ({ dealers, setDealers, showForm, setShowForm }) => {
           </div>
         </div>
       </div>
-
       <div className="dealers_content">
         <div className="dealers_content_left">
           <div className="dealers_title">DEALERS</div>
@@ -48,31 +40,33 @@ export const Dealers = ({ dealers, setDealers, showForm, setShowForm }) => {
             partnerships between homeowners and the best solar dealers in the
             country is what we do best.
           </div>
-
-          <div className="dealers_footer">
-            <div className="dealers_footer_title">
-              What our partners are saying about us:
+        </div>
+      </div>
+      <div className="dealers_bottom">
+        <div className="dealers_bottom_left"></div>
+        <div className="dealers_footer">
+          <div className="dealers_footer_title">
+            What our partners are saying about us:
+          </div>
+          <div className="dealers_footer_content">
+            <div className="dealers_footer_content_item">
+              “This is my first sales job selling anything expencive enough to
+              need financing. I was so worried about being able to understand
+              everything. Prior to today`s conference, I felt in way over my
+              head. But this is a piece of cake. I feel like I`m walking on
+              sunshine.”
             </div>
-            <div className="dealers_footer_content">
-              <div className="dealers_footer_content_item">
-                “This is my first sales job selling anything expencive enough to
-                need financing. I was so worried about being able to understand
-                everything. Prior to today`s conference, I felt in way over my
-                head. But this is a piece of cake. I feel like I`m walking on
-                sunshine.”
-              </div>
-              <div className="dealers_footer_content_item">
-                “I am truly thankful for the way Enium has been able to help me.
-                I have been able togo into homes confident that I am getting the
-                best deal and price per wattage.”
-              </div>
-              <div className="dealers_footer_content_item_last">
-                "Financine with has changedour business and mav he our sinole
-                most imnortant partnershin. They are different than other solar
-                financino companies hecause theu understand thabnsi. ness from
-                the solar companies perspective. Thev put the customer
-                expirience first which trans- lates to sand thine for us."
-              </div>
+            <div className="dealers_footer_content_item">
+              “I am truly thankful for the way Enium has been able to help me. I
+              have been able togo into homes confident that I am getting the
+              best deal and price per wattage.”
+            </div>
+            <div className="dealers_footer_content_item_last">
+              "Financine with has changedour business and mav he our sinole most
+              imnortant partnershin. They are different than other solar
+              financino companies hecause theu understand thabnsi. ness from the
+              solar companies perspective. Thev put the customer expirience
+              first which trans- lates to sand thine for us."
             </div>
           </div>
         </div>
