66302eff642f1e67dcc37e3a37b9dbc8de0c8748
david.shamiiev <david.shamiiev@rad4m.com>
2023-01-09 15:30:05 +0200

    last changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "./fonts/Ghost/TWKGhost-Regular.woff";
+import "./fonts/Everett/TWKEverett-Medium-web.woff";
+import "./fonts/Everett/TWKEverett-Regular-web.woff";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
