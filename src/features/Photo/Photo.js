f3351706f21f44c72ea123095469774caea0f152
david.shamiiev <david.shamiiev@rad4m.com>
2023-01-09 14:24:32 +0200

    last

--- src/features/Photo/Photo.js
+++ src/features/Photo/Photo.js
@@ -4,13 +4,13 @@ import "./Photo.scss";
 export const Photo = () => {
   return (
     <div className="photo_main">
-      {/* <div className="photo-container">
+      <div className="photo-container">
         <img
           className="background_photo"
           src="./assets/images/background.jpg"
           alt="background"
         />
-      </div> */}
+      </div>
     </div>
   );
 };
